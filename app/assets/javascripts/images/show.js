08f91a93e2afb4b5d60839ffd449f7f548429ac9
Adam Kinson <adam.kinson@gmail.com>
2015-09-23 18:34:47 -0400

    add dynamic image_id

--- app/assets/javascripts/images/show.js
+++ app/assets/javascripts/images/show.js
@@ -3,23 +3,58 @@ var WALDO = WALDO || {};
 
 WALDO.ShowModule = (function(){
 
+  var _image_id
   var tags;
   var CHARACTERS;
   var AVAILABLE_CHARACTERS;
 
 
   function init() {
-    tags = [];
+    _image_id = 7;
+    //var promise1 = _getCharacters();
+    //var promise2 = _getTags();
+    //tags = [];
     CHARACTERS = ['Waldo', 'Wenda', 'Odlaw', 'Wizard Whitebeard', 'Woof'];
-    AVAILABLE_CHARACTERS = CHARACTERS;
-    WALDO.Tagger.init(AVAILABLE_CHARACTERS);
+    //AVAILABLE_CHARACTERS = CHARACTERS;
+      WALDO.Tagger.init(AVAILABLE_CHARACTERS);
     _getTags();
   };
 
 
+  function _getCharacters() {
+    $.ajax( {
+      url: "http://localhost:3000/images/" + _image_id + "/characters.json",
+      method: 'get',
+
+      success: function(data) { CHARACTERS = data }
+    });
+  }
+
+
+  function _getTags() {
+    $.ajax( {
+      url: "http://localhost:3000/images/" + _image_id + "/tags.json",
+      method: 'get',
+
+      success: WALDO.Tagger.renderAllSavedTags,
+      error: function() { console.log('error!') }
+    });
+  };
+
+
+  function _availableCharacters() {
+    var charnames = ch.responseJSON.map( function(c) { return c.name} );
+    var tagged = ta.responseJSON.map( function(t) { return t.name} );
+
+    return $.grep(charnames, function(c) {
+      return (tagged.indexOf(c) === -1 )
+    });
+  }
+
+
   function saveTag(tagger) {
     $.ajax( {
-      url: 'http://localhost:3000/images/7/tags.json',
+      url: "http://localhost:3000/images/" + _image_id + "/tags.json",
       method: 'post',
       data: JSON.stringify(tagger),
       dataType: 'json',
@@ -31,20 +66,9 @@ WALDO.ShowModule = (function(){
   };
 
 
-  function _getTags() {
-    $.ajax( {
-      url: 'http://localhost:3000/images/7/tags.json',
-      method: 'get',
-
-      success: WALDO.Tagger.renderAllSavedTags,
-      error: function() { console.log('error!') }
-    });
-  };
-
-
   function deleteTag(id) {
     $.ajax( {
-      url: "http://localhost:3000/images/7/tags/" + id + ".json",
+      url: "http://localhost:3000/images/" + _image_id + "/tags/" + id + ".json",
       method: 'delete',
 
       success: WALDO.Tagger.removeSavedTag,
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :images, :only => [:index, :show] do
     resources :tags, :only => [:create, :index, :destroy]
+    resources :characters, :only => [:index]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
